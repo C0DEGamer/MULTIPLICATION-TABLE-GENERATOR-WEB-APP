6a5bf5ddb29187ef8b9311cc4dda06bb0772ea52
C0DEGamer <154326486+C0DEGamer@users.noreply.github.com>
2024-05-30 15:46:11 +0530

    Update script.js

--- MULTIPLICATION TABLE GENERATOR/script.js
+++ MULTIPLICATION TABLE GENERATOR/script.js
@@ -1,7 +1,7 @@
 let n=prompt("Enter The Number","Any Number");
 let limit=prompt("Enter The Limit","Any");
 let result="";
-  //coded by sreeraj
+   //coded by sreeraj
 for(let i=1;i<=limit;i++){
 
     result+=`${i} x ${n} = ${i*n}\n`;
